859c2ce618cd3b16bcca0dd500f2e235f101c1cb
Foti Ceci <foti.ceci@gmail.com>
2024-05-20 15:11:27 -0400

    create hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+
+import { navItems } from "@/data";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+
 export default function Home() {
   return (
-    <main className="relative ">
-      <div>
-        <h1>Foti Ceci Portfolio</h1>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
       </div>
     </main>
   );
